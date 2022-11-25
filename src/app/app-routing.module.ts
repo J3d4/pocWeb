e215d1f4c0a6957387548ddacab5bc0439839a05
J3d4 <bakos.mrtn@gmail.com>
2022-11-25 12:52:09 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnectionComponent } from './pages/connection/connection.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IntroductionComponent } from './pages/introduction/introduction.component';
+import { MapComponent } from './pages/map/map.component';
+import { SamplesComponent } from './pages/samples/samples.component';
+import { SurveyComponent } from './pages/survey/survey.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'introduction',component:IntroductionComponent},
+  {path:'map',component:MapComponent},
+  {path:'samples',component:SamplesComponent},
+  {path:'survey',component:SurveyComponent},
+  {path:'connection',component:ConnectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IntroductionComponent } from './pages/introduction/introduction.component';
+import { SurveyComponent } from './pages/survey/survey.component';
+import { SamplesComponent } from './pages/samples/samples.component';
+import { MapComponent } from './pages/map/map.component';
+import { ConnectionComponent } from './pages/connection/connection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    IntroductionComponent,
+    SurveyComponent,
+    SamplesComponent,
+    MapComponent,
+    ConnectionComponent
   ],
   imports: [
     BrowserModule,
